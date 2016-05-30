a61b2d3d34802bab286c5cb27a13fa66cb35b6bd
Aluno <Aluno@politecnico.local>
2016-05-30 15:06:25 -0300

    commit

---
+++ src/main/java/br/csi/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package br.csi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClienteController {
+	
+	@RequestMapping("clientes")
+	public String msgTeste(){
+		System.out.println("chamou controlador");
+		return "cliente";
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<form action="spring" method="post">
+<form action="clientes" method="post">
 	<input type="submit" value="teste spring">
 </form>
 </body>
